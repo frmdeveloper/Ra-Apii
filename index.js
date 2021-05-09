8c8981daeeeb9830f7b36d968ece352cf810fee2
I`am Ra <78637964+rayyreall@users.noreply.github.com>
2021-05-09 14:25:51 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const { 
+	RandomCerpen, 
+	emoji,
+	stickerSearch, 
+	ManggaToon,
+	RamalJadian,
+	Pantun, 
+	trutdare,
+	infoFilm123,
+	SearchFilm
+} = require('./lib')
+
+exports.RandomCerpeni = RandomCerpen
+exports.emoji = emoji
+exports.stickerSearch = stickerSearch
+exports.ManggaToon =  ManggaToon
+exports.RamalJadian = RamalJadian
+exports.Pantun = Pantun
+exports.trutdare = trutdare
+exports.infoFilm123 = infoFilm123
+exports.SearchFilm = SearchFilm
